1b4b60d3356100290ae24b780f2199c450c7d9a0
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-09-02 23:55:15 +0300

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import Hero from './Hero'
-import './App.css'
-import Nav from './Nav'
-import Amenity from './Amenity'
-import Why from './Why'
-import International from './international'
-import Destination from './Destination'
-import Packages from './packages'
-function App() {
-  
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Hero from './Hero';
+import Nav from './Nav';
+import Amenity from './Amenity';
+import Why from './Why';
+import International from './International';
+import Destination from './Destination';
+import Packages from './Packages';
+import Review from './Review';
+import Footer from './Footer';
+import Dest from './Dest';
 
+function App() {
   return (
-    
-    <>
-   
-   <Nav />
-      <Hero />
-      <Amenity />
-    
-      <Destination />
-      <Why />
-      <International />
-      <Packages />
-    </>
-  )
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/amenities" element={<Amenity />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/why" element={<Why />} />
+        <Route path="/international" element={<International />} />
+        <Route path="/packages" element={<Packages />} />
+        <Route path="/dest" element={<Dest />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="*" element={<Hero />} /> {/* Default route */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
